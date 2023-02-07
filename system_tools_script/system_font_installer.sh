ca1d2ceae51debec33907293a6269216e6943bc9
E聊 <183186147@qq.com>
2023-02-07 12:25:49 +0000

    update system_tools_script/system_font_installer.sh.
    Signed-off-by: E聊 <183186147@qq.com>

--- system_tools_script/system_font_installer.sh
+++ system_tools_script/system_font_installer.sh
@@ -3,7 +3,7 @@ echo "ready to install chinese fonts."
 sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-sudo pacman	-S	glibc
+sudo pacman	-S --noconfirm	glibc
 sudo sed -i 's/# zh/zh/g'  /etc/locale.gen
 sudo sed -i 's/#zh/zh/g'  /etc/locale.gen
 sudo locale-gen
